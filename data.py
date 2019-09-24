c5dccfea77539047b771c29aa71934a58c0d63a5
JRFrazier <jrfrazier21@gmail.com>
2019-09-24 06:53:08 -0400

    added basic site

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'In a world where Marvel has made hundreds of millions of dollars with a movie staring a talking raccoon and an mobile tree, why not used lorem ipsum text inspired by their movie and comic book summaries? Marvel Ipsum has the same readability problem as Batman Ipsum text, but as an unrepentant Marvel fanboy I couldn’t exclude it.',
+            'author': 'Tony Stark',
+            'create_date': '04-25-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'In a world where Marvel has made hundreds of millions of dollars with a movie staring a talking raccoon and an mobile tree, why not used lorem ipsum text inspired by their movie and comic book summaries? Marvel Ipsum has the same readability problem as Batman Ipsum text, but as an unrepentant Marvel fanboy I couldn’t exclude it.',
+            'author': 'Thor',
+            'create_date': '04-25-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'In a world where Marvel has made hundreds of millions of dollars with a movie staring a talking raccoon and an mobile tree, why not used lorem ipsum text inspired by their movie and comic book summaries? Marvel Ipsum has the same readability problem as Batman Ipsum text, but as an unrepentant Marvel fanboy I couldn’t exclude it.',
+            'author': 'Bruce Banner',
+            'create_date': '04-25-2019'
+        }
+    ]
+    return articles
