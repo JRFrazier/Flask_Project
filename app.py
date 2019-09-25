879010ad5afd68b58bfbeb1d0881229c4acf8ccc
JR <Frazier>
2019-09-25 06:30:44 -0400

    Added ability to add articles and added text editor

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ Articles = Articles()
 # Home Page
 @app.route('/')
 def index():
-    return render_template('home.jinja')
+    return render_template('home.html')
 
 # About Page
 @app.route('/about')
 def about():
-    return render_template('about.jinja')
+    return render_template('about.html')
 
 # All Articles
 @app.route('/articles')
@@ -49,98 +49,130 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
-    # User Register
-    @app.route('/register', methods=['GET', 'POST'])
-    def register():
-        form = RegisterForm(request.form)
-        if request.method == 'POST' and form.validate():
-            name = form.name.data
-            email = form.email.data
-            username = form.username.data
-            password = sha256_crypt.encrypt(str(form.password.data))
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
 
-            # Create Cursor
-            cur = mysql.connection.cursor()
+        # Create Cursor
+        cur = mysql.connection.cursor()
 
-            # Execute query
-            cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
-                        (name, email, username, password))
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
 
-            # Commit to DB
-            mysql.connection.commit()
+        # Commit to DB
+        mysql.connection.commit()
 
-            # Close connection
-            cur.close()
+        # Close connection
+        cur.close()
 
-            flash('You are now registered and can log in', 'success')
+        flash('You are now registered and can log in', 'success')
 
-            return redirect(url_for('login'))
-
-        return render_template('register.html', form=form)
-
-    # Login
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        if request.method == 'POST':
-            # Get Form Fields
-            username = request.form['username']
-            password_candidate = request.form['password']
-
-            # Create cursor
-            cur = mysql.connection.cursor()
-
-            # Get user by username
-            result = cur.execute(
-                'SELECT * FROM users WHERE username = %s', [username])
-
-            if result > 0:
-                # Get stored hash
-                data = cur.fetchone()
-                password = data['password']
-
-                # Compare Passwords
-                if sha256_crypt.verify(password_candidate, password):
-                    # Passed
-                    session['logged_in'] = True
-                    session['username'] = username
-
-                    flash('You are now logged in', 'success')
-                    return redirect(url_for('dashboard'))
-                else:
-                    error = 'Invalid Password'
-                    return render_template('login.html', error=error)
-                # Close connection
-                cur.close()
-            else:
-                error = 'Username not found'
-                return render_template('login.html', error=error)
-
-        return render_template('login.html')
-
-    # Check if logged in
-
-    def is_logged_in(f):
-        @wraps(f)
-        def wrap(*args, **kwargs):
-            if 'logged_in' in session:
-                return f(*args, **kwargs)
-            else:
-                flash('Unauthorized, Please login', 'danger')
-                return redirect(url_for('login'))
-        return wrap
-
-    # Logout
-    @app.route('/logout')
-    def logout():
-        session.clear()
-        flash('You are now logged out', 'success')
         return redirect(url_for('login'))
 
-    # Dashboard
-    @app.route('/dashboard')
-    @is_logged_in
-    def dashboard():
-        return render_template('dashboard.html')
+    return render_template('register.html', form=form)
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute(
+            'SELECT * FROM users WHERE username = %s', [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Password'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+# Check if logged in
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+# Article From Class
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=30)])
+
+# Add Article
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Execute
+        cur.execute('INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)', (title, body, session['username']))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('Article Created', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
 
 
 if __name__ == '__main__':
